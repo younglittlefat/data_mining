b453b6d4118c790afb8b6b030defbcbdb585f32a
younglittlefat <younglittlefat@gmail.com>
2018-09-09 08:58:48 +0800

    update

--- financial_report/eval/train.py
+++ financial_report/eval/train.py
@@ -6,7 +6,7 @@ import random
 import pickle
 
 import numpy as np
-from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestClassifier
 from sklearn.neural_network import MLPRegressor
 from sklearn import metrics
 #from sklearn.preprocessing import MinMaxScaler
@@ -28,34 +28,22 @@ class TrainProcess:
 
         """
         self.raw_train_list = []
+        self.raw_train_label = []
         self.raw_test_list = []
+        self.raw_test_label = []
         with codecs.open(self.train_file, "r", "utf-8", "ignore") as f:
-            for line in f:
-                temp = line.strip().split(" ")
-                self.raw_train_list.append(temp[2:])
+            self.raw_train_list = [temp[2:-1] for temp in map(lambda x:x.strip().split(" "), f.readlines())]
+        with codecs.open(self.train_file, "r", "utf-8", "ignore") as f:
+            self.raw_train_label = [1 if float(temp[-1]) > 0.15 else 0 for temp in map(lambda x:x.strip().split(" "), f.readlines())]
 
         print "Length of raw_train:%d" % (len(self.raw_train_list))
 
         with codecs.open(self.test_file, "r", "utf-8", "ignore") as f:
-            for line in f:
-                temp = line.strip().split(" ")
-                self.raw_test_list.append(temp[2:])
+            self.raw_test_list = [temp[2:-1] for temp in map(lambda x:x.strip().split(" "), f.readlines())]
+        with codecs.open(self.test_file, "r", "utf-8", "ignore") as f:
+            self.raw_test_label = [1 if float(temp[-1]) > 0.15 else 0 for temp in map(lambda x:x.strip().split(" "), f.readlines())]
 
         print "Length of raw_test:%d" % (len(self.raw_test_list))
-
-
-    def _label_normalization(self, data_list):
-        """
-
-        """
-        label_min = min(map(lambda x:x[-1], data_list))
-        label_max = max(map(lambda x:x[-1], data_list))
-        print "label min:%.2f" % label_min
-        print "label max:%.2f" % label_max
-        for feat in data_list:
-            feat[-1] = (feat[-1] - label_min) / (label_max - label_min)
-
-        return data_list
 
 
     def normalization(self):
@@ -64,26 +52,28 @@ class TrainProcess:
         """
         #self.raw_data_list = self._label_normalization(self.raw_data_list)
         train_scaler = MinMaxScaler()
-        self.raw_train_list = train_scaler.fit_transform(self.raw_train_list)
-        self.raw_test_list = train_scaler.transform(self.raw_test_list)
+        norm_train_list = train_scaler.fit_transform(self.raw_train_list)
+        norm_test_list = train_scaler.transform(self.raw_test_list)
         #with open("scaler", "wb") as f:
         #    pickle.dump(scaler, f)
-
+        return norm_train_list, norm_test_list
 
 
     def train(self):
         """
 
         """
-        self.normalization()
+        norm_train, norm_test = self.normalization()
+        train_label = np.array(self.raw_train_label)
+        test_label = np.array(self.raw_test_label)
 
         #model = GradientBoostingRegressor(verbose = 1, max_depth = 10, n_estimators = 200, loss = "lad")
-        model = RandomForestRegressor(verbose = 1, n_estimators = 100, n_jobs = -1)
+        model = RandomForestClassifier(verbose = 1, n_estimators = 300, n_jobs = -1)
         #model = MLPRegressor(verbose = True, hidden_layer_sizes = (30, 15))
 
         # training
-        model.fit(self.raw_train_list[:, :-1], self.raw_train_list[:, -1])
-        print model.score(self.raw_test_list[:, :-1], self.raw_test_list[:, -1])
+        model.fit(norm_train, train_label)
+        print model.score(norm_test, test_label)
 
         """
         with open("model", "wb") as f:
