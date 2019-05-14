02fc3d69836016bbbfe6275e8a780d64ec92d529
younglittlefat <younglittlefat@gmail.com>
2019-05-15 01:42:12 +0800

    update proxy_factory.py

--- financial_report/digger/proxy/proxy_factory.py
+++ financial_report/digger/proxy/proxy_factory.py
@@ -3,6 +3,8 @@ import sys
 import urllib2 as u2
 import json
 import logging
+import time
+import random
 
 import requests
 
@@ -18,6 +20,7 @@ class ProxyFactory:
         self.user_agent = "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) Gecko/20100101 Firefox/55.0"
         self.headers["User-Agent"] = self.user_agent
         self.ip_pool = []
+        self.ip_idx = 0
         
         # 读代理api地址
         with open("address.txt", "r") as f:
@@ -29,29 +32,70 @@ class ProxyFactory:
         self._get_new_ips()
 
 
-    def _ip_info_maker(ip, port, expire_time):
+    def _ip_info_maker(self, ip, port, expire_time):
         """
-        
+        构造一个基本的IP信息，包含ip，port，过期时间，失败次数        
         """
         d = {}
-        d
+        d["ip"] = ip
+        d["port"] = port
+        d["expire_time"] = expire_time
+        d["fail_times"] = 0
+        return d
+
+
+    def _check_ip_available(self):
+        """
+        查看是否所有ip都失效了，如果是则返回False
+        失效标准为失败次数大于等于5
+        """
+        still_valid = False
+        for ip_info in self.ip_pool:
+            if ip_info["fail_times"] < 5:
+                still_valid = True
+                break
+        return still_valid
+
 
     def _get_new_ips(self):
         """
         获取新的一批IP
         """
-        new_ip_pool = []
+        time.sleep(2)
         res = requests.get(self.api_addr, self.headers)
         j = json.loads(res.content)
+        print json.dumps(j, ensure_ascii = False)
         for ele in j["data"]:
-            new_ip = proxy_tuple(ele["ip"], ele["port"], ele["expire_time"])
+            new_ip = self._ip_info_maker(ele["ip"], ele["port"], ele["expire_time"])
             logging.info(new_ip)
-            new_ip_pool.append(new_ip)
+            self.ip_pool.append(new_ip)
 
 
-    def get_one_ip():
-        pass
+    def get_one_ip(self):
+        if not self._check_ip_available():
+            logging.info("All ips are unavailable. Now re-fetching ips")
+            self.ip_pool = []
+            self.ip_idx = 0
+            self._get_new_ips()
+
+        get_valid_ip = False
+        now_ip = None
+        while not get_valid_ip:
+            now_ip = self.ip_pool[self.ip_idx]
+            self.ip_idx += 1
+            if self.ip_idx >= 10:
+                self.ip_idx = 0
+            if now_ip["fail_times"] < 5:
+                logging.info("Now ip:" + json.dumps(now_ip))
+                get_valid_ip = True
+            else:
+                continue
+        return now_ip
 
 
 if __name__ == "__main__":
     pf = ProxyFactory()
+    for i in range(500):
+        now_ip = pf.get_one_ip()
+        if random.random() < 0.5:
+            now_ip["fail_times"] += 1
